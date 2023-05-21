b2fa0b3fd01340b8a87be1cf8292f796c3560336
Sim-GyuBin <skb0606@gmail.com>
2023-05-21 13:59:19 +0900

    [심규빈] 업데이트 시작

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,7 +4,7 @@ const model = require("../models/model");
 exports.mainview = (req, res) => {
     //상대경로 사용할 것 (팀원들 각자 디렉토리 다르니 절대경로 안돼)
     //index.ejs 렌더링 및 변수 ejs에 넘기기
-    model.getTeam(function( result ) {
+    model.getAllTeam(function( result ) {
         res.render(path.join(__dirname + '/../views/main.ejs'), {
             title: "testtitle",
             Team: result
@@ -12,14 +12,10 @@ exports.mainview = (req, res) => {
         console.log(result);
     });
 
-    // res.render(path.join(__dirname + '/../views/main.ejs'), {
-    //     title: "TestTemplate",
-    //     Team: model.getTeam((error, result)=>{
-    //         if (error) {
-    //             res.status(500).send('Internal Server Error');
-    //         } else {
-    //             res.send(result);
-    //         }    
-    //     })
-    // });
+    model.getOneTeam(2, function( result ) {
+        res.render(path.join(__dirname + '/../views/main.ejs'), {
+            Teamone: result
+        });
+    });
+
 };
