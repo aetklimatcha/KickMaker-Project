fe60d0a03968f2689909b27940c4f3b22695c264
Sim-GyuBin <skb0606@gmail.com>
2023-05-23 21:24:16 +0900

    [귭] ejs 렌딩 서버

--- render.js
+++ render.js
@@ -4,11 +4,13 @@ var app = express();
 app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 
+
+
 app.get('/',function(req,res){
+    //아래 경로 조정해서 띄울 ejs 설정!!!!!!!
     res.render(__dirname + '/views/register.ejs');
 });
 
-// 서버
 app.listen(3000, () => {
     console.log(`Example app listening on port 3000`)
 });
