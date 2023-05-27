dec8ce38a80ed2318b70c07a8686487bffc2ebaf
JiyunHJY <hwangjiyun7377@gmail.com>
2023-05-27 19:09:44 +0900

    [황] 회원가입한 정보로 로그인

--- models/signup.js
+++ models/signup.js
@@ -6,9 +6,9 @@ const mysql = require("../config/mysql");
 module.exports = {
 
 
-    //회원가입 시 Team에 회원 정보(아이디,비밀번호,팀이름,팀대표자명,연락처,로고아이디) 삽입
-    insertsignup: function ( id, password, teamname, represent_name, hp,  logo_id, callback ) {
-        const querystring = `INSERT INTO Team ( id, password, teamname, represent_name, hp, logo_id ) VALUES ( '${id}', '${password}', '${teamname}', '${represent_name}', '${hp}', '${logo_id}');`;
+    //회원가입 시 Team에 회원 정보(아이디,비밀번호,팀이름,팀대표자명,연락처) 삽입
+    insertsignup: function ( id, password, teamname, represent_name, hp, callback ) {
+        const querystring = `INSERT INTO Team ( id, password, teamname, represent_name, hp) VALUES ( '${id}', '${password}', '${teamname}', '${represent_name}', '${hp}');`;
         mysql.query(querystring, (err, rows) => {
             if ( err ) throw err;
             console.log( rows ); 
