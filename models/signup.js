1cdddd608c614aab3d823bc6e94f19a43a6240bb
JiyunHJY <hwangjiyun7377@gmail.com>
2023-05-27 17:33:59 +0900

    [황] 오류 수정

--- models/signup.js
+++ models/signup.js
@@ -7,8 +7,8 @@ module.exports = {
 
 
     //회원가입 시 Team에 회원 정보(아이디,비밀번호,팀이름,팀대표자명,연락처,로고아이디) 삽입
-    insertsignup: function ( user_id, password, teamname, represent_name, hp,  logo_id, callback ) {
-        const querystring = `INSERT INTO Team ( user_id, password, teamname, represent_name, hp, logo_id ) VALUES ( '${user_id}', '${password}', '${teamname}', '${represent_name}', '${hp}', '${logo_id}');`;
+    insertsignup: function ( id, password, teamname, represent_name, hp,  logo_id, callback ) {
+        const querystring = `INSERT INTO Team ( user_id, password, teamname, represent_name, hp, logo_id ) VALUES ( '${id}', '${password}', '${teamname}', '${represent_name}', '${hp}', '${logo_id}');`;
         mysql.query(querystring, (err, rows) => {
             if ( err ) throw err;
             console.log( rows ); 
