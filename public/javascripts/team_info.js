f274e70c384189649293cd0eeece4cb171bf3c9f
Evelyn <89955014+aetklimatcha@users.noreply.github.com>
2023-05-27 14:36:45 +0900

    [현수] 마이페이지-팀프로필, 승률 확인

---
+++ public/javascripts/team_info.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // 승률 점수 차트
+  var winRateChart = new Chart(document.getElementById('winRateChart'), {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [60, 40], // 승률 점수와 나머지 비율 (임의의 값)
+        backgroundColor: ['#3f51b5', '#f2f2f2'],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: '승률',
+          font: {
+            size: 16,
+            weight: 'bold'
+          },
+          color: '#000',
+          padding: {
+            top: 10,
+            bottom: 30
+          }
+        }
+      }
+    }
+  });
+
+  // 매너 점수 차트
+  var mannerChart = new Chart(document.getElementById('mannerChart'), {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [80, 20], // 매너 점수와 나머지 비율 (임의의 값)
+        backgroundColor: ['#3f51b5', '#f2f2f2'],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: '매너 점수',
+          font: {
+            size: 16,
+            weight: 'bold'
+          },
+          color: '#000',
+          padding: {
+            top: 10,
+            bottom: 30
+          }
+        }
+      }
+    }
+  });
+});
