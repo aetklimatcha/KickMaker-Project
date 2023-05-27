8480eb9591f436fe69bcea85b5b0c9a85373073b
JiyunHJY <hwangjiyun7377@gmail.com>
2023-05-27 16:44:35 +0900

    [황] 회원가입 테스트

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const res = require('express');
 const connection = require('../config/mysql');
 const main = require('../controllers/main');
 const login = require('../controllers/login')
+const signup = require("../controllers/signup");
 
 const { cookieJwtAuth } = require('../middleware/cookieJwtAuth');
+
 //url이 빈 상태로 넘어올 경우
 router.get('/',main.mainview);
 
@@ -27,8 +29,10 @@ router.get('/team_info',cookieJwtAuth, main.team_infoview);
 router.post('/createteam', main.createteam);
 router.post('/login',login.login_process);
 router.post('/logout',login.logout);
+router.post('/signup', signup.signup);
 
 //규빈 테스트용 페이지
+
 router.get('/test',cookieJwtAuth,main.testsview);
 router.post('/test', cookieJwtAuth,main.testview);
 
