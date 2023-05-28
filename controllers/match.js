6a780d175dfd73d267933b03796602b67cf3040d
Sim-GyuBin <skb0606@gmail.com>
2023-05-29 02:56:30 +0900

    [규빈] 매치메이킹(시간,장소) 및 추려진 매치 쿠키 전달

--- controllers/match.js
+++ controllers/match.js
@@ -2,36 +2,56 @@ const path = require("path");
 const model = require("../models/Match");
 const findMatch = require("../modules/findMatch");
 const { match } = require("assert");
+const jwt = require('jsonwebtoken');
+const secretKey = require('../config/secretkey').secretKey;
+const options = require('../config/secretkey').options;
 require('dotenv').config();
 
 module.exports = {
 
     match_making : (req, res) => {
         
-        
-        console.log(req.user_id);
-        var match_place = req.body.district;
-        
-        var matchDay = req.body.gameDay;
+        var match_place = req.body.district;       
+        var gameDate = req.body.gameDate;
         var match_time_start = req.body.gameStartTime;
         var match_time_end = req.body.gameEndTime;
 
+
+
+        var info = {
+            place:match_place,
+            date:gameDate,
+            starttime:match_time_start,
+            endtime:match_time_end
+        };
+
+        findMatch.findMatch(info, (matchData, matchAvailability)=>{
+            //results : 경기 가능 팀들의 {id,가능장소,겹치는시간}
+            //matchAvailability : 경기할 팀 여부 true: 있음, false: 없음
+            payload = matchData;
+            payload = JSON.stringify(payload);
+            console.log(JSON.parse(payload));
+            //매치가 없는 경우
+            if (matchAvailability==false) {
+                token = jwt.sign(payload,secretKey,options);
+                res.cookie('matchtoken',token);
+                res.redirect('/noMatch');
+            } else if (matchAvailability==true) {
+                token = jwt.sign(payload,secretKey,options);
+                res.cookie('matchtoken',token);
+                res.redirect('/');
+            }
+        });
+    },
+
+    noMatch : (req,res) => {
+
         var currentDate = new Date();
         var created = currentDate.toISOString().replace('T', ' ').substr(0, 19);
-        console.log(created);
+        //nomatch 페이지에서 등록하기 누를 경우 실행할 것
+        model.insertMatch(req.user_id, gameDate,match_place, created,match_time_start,match_time_end,function( result ) { 
 
-        match_time_start = matchDay+" "+match_time_start;
-        match_time_end = matchDay+" "+match_time_end;
-        console.log(match_time_start);
-        console.log(match_time_end);
+        });  
 
-        // model.insertMatch(req.user_id, match_place, created,match_time_start,match_time_end,function( result ) { 
-        //     var matchStatus = findMatch.findMatch(result);
-        //     if (matchStatus) {
-        //         res.redirect('/')
-        //     }else {
-
-        //     }
-        // });          
     }
 }
--- middleware/cookieJwtAuth.js
+++ middleware/cookieJwtAuth.js
@@ -3,16 +3,20 @@ const secretKey = require('../config/secretkey').secretKey;
 const options = require('../config/secretkey').options;
 
 exports.cookieJwtAuth = (req, res, next) => {
-  const { token } = req.cookies;
+  const { usertoken } = req.cookies;
+  const { matchtoken } = req.cookies;
   try {
-    const user = jwt.verify(token,secretKey); // 검증
+    const user = jwt.verify(usertoken,secretKey); // 검증
+    const match = jwt.verify(matchtoken,secretKey); // 검증
     req.user_id = user;
+    req.match = match;
+    console.log(req.user_id);
+    console.log(req.match);
       next();
   } catch (err) {
     res.clearCookie('token');
     console.log("cookie 없는데요?");
     req.user_id = null;
       next();
-    //res.redirect('/test');
   }
 };
