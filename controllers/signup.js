fa164630160bac28a525bc0afaccd049fcb993d4
Sim-GyuBin <skb0606@gmail.com>
2023-07-15 16:31:18 +0900

    [규빈] 기본이미지 설정

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,6 +5,8 @@ const model = require("../models/signup");
 
 module.exports= {
     signup : (req, res) => {
+        if(req.file.filename == null) 
+            req.file.filename = 'default.jpg';
         model.insertsignup(req.body.id, req.body.password, req.body.teamname, req.body.represent_name, req.body.hp, req.file.filename,function( result ) {
             res.redirect('/')
         });  
