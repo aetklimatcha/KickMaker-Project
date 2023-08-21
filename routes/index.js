5e28ba339954260f6f8126d29e07fba32ba85583
Sim-GyuBin <skb0606@gmail.com>
2023-08-22 00:57:04 +0900

    routes분리

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const req = require('http');
-const res = require('express');
-const connection = require('../config/mysql');
+
 const main = require('../controllers/main');
-const login = require('../controllers/login')
 const signup = require("../controllers/signup");
-const match = require("../controllers/match");
+const login = require('../controllers/login');
 
 const { cookieJwtAuth } = require('../middleware/cookieJwtAuth');
+const { header } = require('../middleware/header');
+const { uploadMiddleware } = require('../middleware/uploadMiddleware');
 
 const team = require('./team');
 const game = require('./game');
 const matching = require('./matching');
 
 //ejs 파일들 연결 페이지
-router.get('/', cookieJwtAuth,main.mainview);
-router.get('/match-list',cookieJwtAuth, main.match_listview);
-router.get('/match-making', cookieJwtAuth,main.match_makingview);
-router.get('/noMatch',cookieJwtAuth, main.noMatchview);
-router.get('/matched', cookieJwtAuth,main.matchedview);
-router.get('/signin',cookieJwtAuth, main.signinview)
-router.get('/signup',cookieJwtAuth, main.signupview);
-router.get('/complete',cookieJwtAuth, main.completeview);
+/**
+ * @swagger
+ * paths:
+ *  /:
+ *    get:
+ *      summary: "메인 페이지 조회"
+ *      description: "서버에 데이터를 보내지 않고 Get방식으로 요청"
+ *      tags: [메인]
+ *      responses:
+ *        "200":
+ *          description: 전체 유저 정보
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    ok:
+ *                      type: boolean
+ *                    users:
+ *                      type: object
+ *                      example:
+ *                          [
+ *                            { "id": 1, "name": "유저1" },
+ *                            { "id": 2, "name": "유저2" },
+ *                            { "id": 3, "name": "유저3" },
+ *                          ]
+ */
+// main
+router.get('/', cookieJwtAuth,header,main.mainview);
+router.get('/signin',cookieJwtAuth,header, main.signinview)
+router.get('/signup',cookieJwtAuth,header, main.signupview);
 
-
-router.post('/tomain', cookieJwtAuth,main.tomain);
-router.post('/createteam', main.createteam);
 router.post('/login',login.login_process);
 router.post('/logout',login.logout);
-router.post('/signup', signup.signup);
-router.post('/match-making', cookieJwtAuth,match.match_making);
-router.post('/complete', cookieJwtAuth,match.insertMatch);
-//규빈 테스트용 페이지
+router.post('/signup', uploadMiddleware,signup.signup);
+router.post('/tomain', cookieJwtAuth,header,main.tomain);
 
-router.get('/test',cookieJwtAuth,main.testsview);
-router.post('/test', cookieJwtAuth,main.testview);
+//test page
+router.get('/maptest', cookieJwtAuth,header,main.maptestview);
+router.post('/upload',main.upload);
+
+router.use('/team', team);
+router.use('/game', game);
+router.use('/matching', matching);
 
 module.exports = router;
